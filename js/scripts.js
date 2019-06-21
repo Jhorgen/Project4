fae1ee60fd4e7b24fd4b6947c82944c0ac9f7f93
Josh <Joshua.horgen@gmail.com>
2019-06-21 08:53:42 -0700

    Add constructor, js structure

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+//-----------------------------------BUSINESS LOGIC---------------------------//
+
+// Holds total customer order information
+function TotalOrder(add, add, add) {
+  this.
+  this.
+  This.
+}
+
+// Template for each pizza
+function PizzaConstructor(add, add, add) {
+  this.
+  this.
+  this.
+  this.
+};
+
+TotalOrder.prototype.addOrder = function (PizzaConstructor) {
+  //
+
+};
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------UI lOGIC----------------------------------//
+
+$(document).ready(function(){
+  var orderForm = new TotalOrder();
+});
