043c4ee52524d45a588405628779951ed27cbfa0
Josh <Joshua.horgen@gmail.com>
2019-06-30 19:27:53 -0700

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,44 @@
 function TotalOrder() {
   this.orders = [];
   console.log(this.orders);
+  this.currentId = 0
+
 };
 
-function PizzaConstructor(inputSize, toppings) {
-  this.size = inputSize,
-  this.toppings = toppings;
+function PizzaConstructor(totalCost) {
+  this.cost = totalCost
 };
 
 TotalOrder.prototype.addOrder = function (addPizza) {
   this.orders.push(addPizza);
+  this.currentId += 1;
 };
+
 
 //----------------------------------UI lOGIC----------------------------------//
 
-// var orderForm = new TotalOrder();
-
-// var inputSize = $("input[name='pizzaSize']:checked").val();
-//
-// var toppings = $("input[name='pizzaTopping']:checked").val();
+var orderForm = new TotalOrder();
 
 $(document).ready(function () {
   $('.formDiv').submit(function (event) {
     event.preventDefault();
-    var inputSize = $('input:radio[name=pizzaSize]:checked').val();
-    console.log(inputSize);
+    var inputSize = parseInt($('input:radio[name=pizzaSize]:checked').val(), 10);
 
-    var toppings = $('input[name=pizzaTopping]:checked').each(function () {
-      orderForm.newPizza.addOrder($(this).val());
-      console.log(toppings);
-
-      var newPizza = new PizzaConstructor(inputSize, toppings);
+    var toppings = 0
+    $("#theseCheck input[type='checkbox']:checked").each(function (){
+      toppings += parseInt(this.value, 10);
     });
+
+    const total = inputSize + toppings;
+
+    var newPizza = new PizzaConstructor(totalCost);
+
+    orderForm.addOrder(newPizza)
+
+    console.log(newPizza);
+
+    $('.output').text(orderForm.orders);
+
 
   });
 });
