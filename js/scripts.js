8793cb6de60e427483c637216d6ce185979694b3
Josh <Joshua.horgen@gmail.com>
2019-12-22 13:56:12 -0800

    Add conditional rendering for submit button

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,27 @@ PizzaConstructor.prototype.pizzaCost = function() {
   return this.size + this.toppings;
 }
 
+$(document).ready(function () {
+  let pizzaSizeCheck = document.getElementById('pizzaSize');
+  let toppingsCheck = document.getElementById('toppings');
+
+  $('#pizzaSize').click(function () {
+    $(this).addClass("clicked");
+  })
+
+  $('#toppings').click(function () {
+    $(this).addClass("clicked");
+    if(pizzaSizeCheck.classList.contains('clicked') && toppingsCheck.classList.contains('clicked')) {
+      console.log('yes');
+      $('#total-button').css('display', 'block');
+    } else {
+      console.log('no');
+    }
+  })
 
 
+
+})
 //----------------------------------UI lOGIC----------------------------------//
 
 var orderForm = new TotalOrder();
@@ -29,14 +48,14 @@ $(document).ready(function () {
     var inputSize = parseInt($('input:radio[name=pizzaSize]:checked').val(), 10);
 
     var toppings = 0
-    $("#theseCheck input[type='checkbox']:checked").each(function (){
+    $("#toppings input[type='checkbox']:checked").each(function (){
       toppings += parseInt(this.value, 10);
     });
 
     var newPizza = new PizzaConstructor(inputSize, toppings);
     orderForm.addOrder(newPizza);
 
-    $('.output').text(newPizza.pizzaCost());
+    $('.output').text('Total:' + ' ' + newPizza.pizzaCost());
 
   });
 });
